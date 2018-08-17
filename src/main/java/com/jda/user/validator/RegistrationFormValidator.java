310e4131554715a81fc2f0758de4c106c1cf7fb8
mohanpednekar <pednekar.mohan@gmail.com>
2018-08-17 19:42:07 +0530

    Log4j added

--- src/main/java/com/jda/user/validator/RegistrationFormValidator.java
+++ src/main/java/com/jda/user/validator/RegistrationFormValidator.java
@@ -15,13 +15,13 @@ public class RegistrationFormValidator implements Validator {
 	public void validate(Object target, Errors errors) {
 
 		User user = (User) target;
-
+		if (!(user.getUsername().matches("[a-zA-Z ]*$"))) {
+			errors.rejectValue("username", "symbolsPresent", new Object[]{"'username'"}, "name can't be symbols");
+		}
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "username", "username.required", "Name is required");
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "password.required", "Password is required");
 		ValidationUtils.rejectIfEmpty(errors, "phone", "phone.required", "Phone is required");
 		ValidationUtils.rejectIfEmpty(errors, "email", "email.required", "Email is required");
-		if (!(user.getUsername().matches("[a-zA-Z ]*$"))) {
-			errors.rejectValue("username", "symbolsPresent", new Object[]{"'username'"}, "name can't be symbols");
-		}
+		ValidationUtils.rejectIfEmpty(errors, "address", "address.required", "");
 	}
 }
